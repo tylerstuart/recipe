bd2fd1a0821bb405af4836310fab576e7ff4a9a7
Tyler Stuart <tstuart@1on1.com>
2012-06-07 16:19:34 -0600

    finished user edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120607155514) do
+ActiveRecord::Schema.define(:version => 20120607205715) do
 
   create_table "recipe_alls", :force => true do |t|
     t.string   "recipe"
@@ -28,10 +28,11 @@ ActiveRecord::Schema.define(:version => 20120607155514) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
